e0219b7a1e12d6c9668720fce228b12d8e591a65
roboshed <crobicha@gmail.com>
2013-11-08 12:41:19 -0500

    Setting up db info

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
- 
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => "mysql",
+  :host     => "mysql.play-smarter.net",
+  :username => "play_smarter",
+  :password => "BYS2avW4",
+  :database => "play_smarter_staging"
+)
+
+#class Game < ActiveRecord::Base
+#end
+
+#class Event < ActiveRecord::Base
+#end
+
+class User < ActiveRecord::Base
+	
+end
+
+
+
+class App < Sinatra::Application
+end
+
 get '/' do
-    "Hello, World! HI PEPE"
+  p User.all
 end
